a1b518a57a3e64a144c2286df644c567dbf1bc6b
massu-159 <magjtwjp@gmail.com>
2023-09-25 21:28:05 +0900

    add functions

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,61 @@
-import Image from 'next/image'
+'use client'
+import { memo, useState, useMemo, useCallback } from 'react'
 import styles from './page.module.css'
 
-export default function Home() {
+
+// コンポーネントのメモ化
+const Child_1 = () => {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <>
+      <p>子コンポーネント１です。</p>
+    </>
+  )
+}
+const MemoChild_1 = memo(Child_1)
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+const Child_2 = ( props: { handleClick: () => void }) => {
+  return (
+    <>
+      <p>子コンポーネント2です。</p>
+      <button onClick={props.handleClick}>ボタン</button>
+    </>
+  )
+}
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+export default function Parent() {
+  const [text, setText] = useState('')
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value)
+  }
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+  // コールバック関数のメモ化
+  const handleClick = useCallback(() => {
+    console.log('ボタンがクリックされました。')
+  },[])
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+  const [count, setCount] = useState(0)
+
+  const double = (count: number) => {
+    let i = 0
+    while (i < 1000000000) {
+      i++
+    }
+    return count * 2
+  }
+  // const doubleCount = double(count) //重い計算処理 
+  // 処理のメモ化
+  const doubleCount = useMemo(()=>double(count),[count]) //メモ化
+
+  return (
+    <div>
+      <p>親コンポーネントです。</p>
+      <input type="text" onChange={handleInputChange} value={text} />
+      <MemoChild_1 />
+      <Child_2 handleClick={handleClick} />
+      <p>親コンポーネントで重い計算処理</p>
+      <p>Counter:{count} , {doubleCount}</p>
+      <button onClick={()=>setCount(count + 1)}>Increment Count</button>
+    </div>
   )
 }
